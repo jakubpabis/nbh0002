8832c1390b8ed30fa7b9ea9fd10224e46a5cfb51
Sative <jakub@sative.co.uk>
2021-09-13 16:28:58 +0100

    dsfg

--- build/wp-content/themes/nbhd/flexparts/icons.php
+++ build/wp-content/themes/nbhd/flexparts/icons.php
@@ -1,9 +1,9 @@
 <?php if (have_rows('cards')) : ?>
 	<section class="cards my-4 py-3">
 		<div class="container-xl">
-			<div class="row">
+			<div class="row justify-content-center">
 				<?php while (have_rows('cards')) : the_row(); ?>
-					<div class="col-md-4">
+					<div class="col-md-4 col-sm-8 mb-md-0 mb-4">
 						<div class="cards__card bg-grey d-flex justify-content-center align-items-center py-lg-5 py-4 flex-wrap text-center">
 							<?php if (get_sub_field('icon')) : ?>
 								<img src="<?php echo get_sub_field('icon')['url']; ?>" alt="<?php echo get_sub_field('icon')['title']; ?>" class="w-25 mb-4">
