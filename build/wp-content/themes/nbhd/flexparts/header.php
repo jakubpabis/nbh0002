b94112fc63c81e2258a247237c560c0debe9d0a9
Sative <jakub@sative.co.uk>
2021-09-13 11:15:15 +0100

    sdfgsdfg

--- build/wp-content/themes/nbhd/flexparts/header.php
+++ build/wp-content/themes/nbhd/flexparts/header.php
@@ -2,7 +2,7 @@
 	<div class="nbhd-products-archive with_header_img">
 		<div class="container-fluid">
 			<?php woocommerce_breadcrumb(); ?>
-			<header class="woocommerce-products-header with_header_img">
+			<header class="woocommerce-products-header with_header_img <?php echo get_sub_field('height'); ?>">
 				<img class="lazy bg-cover loaded m-0" alt="<?php echo get_sub_field('image')['title']; ?>" src="<?php echo get_sub_field('image')['url']; ?>">
 				<h1 class="woocommerce-products-header__title page-title with_header_img">
 					<?php echo get_sub_field('title'); ?>
