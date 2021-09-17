e1ea10ab43ec562b6865687e6d7b4b1bc4915a92
Sative <jakub@sative.co.uk>
2021-09-18 00:27:51 +0100

    some

--- build/wp-content/themes/nbhd/tpl_home.php
+++ build/wp-content/themes/nbhd/tpl_home.php
@@ -4,15 +4,13 @@
  * Template Name: Homepage
  */
 
-get_header();
-
-if (get_field('slides')) {
-	get_template_part('template-parts/content', 'slider');
-} ?>
+get_header(); ?>
 
 <?php if (have_rows('sections')) : ?>
 	<?php while (have_rows('sections')) : the_row(); ?>
-		<?php if (get_row_layout() == 'promo_with_cards') : ?>
+		<?php if (get_row_layout() == 'header') : ?>
+			<?php get_template_part('flexparts/header'); ?>
+		<?php elseif (get_row_layout() == 'promo_with_cards') : ?>
 			<?php get_template_part('flexparts/promo'); ?>
 		<?php elseif (get_row_layout() == 'cards_with_icons') : ?>
 			<?php get_template_part('flexparts/icons'); ?>
@@ -20,6 +18,12 @@ if (get_field('slides')) {
 			<?php get_template_part('flexparts/products'); ?>
 		<?php elseif (get_row_layout() == 'image_and_text') : ?>
 			<?php get_template_part('flexparts/image-text'); ?>
+		<?php elseif (get_row_layout() == 'slider') : ?>
+			<?php get_template_part('flexparts/slider'); ?>
+		<?php elseif (get_row_layout() == 'team') : ?>
+			<?php get_template_part('flexparts/team'); ?>
+		<?php elseif (get_row_layout() == 'marki') : ?>
+			<?php get_template_part('flexparts/marki'); ?>
 		<?php endif; ?>
 	<?php endwhile; ?>
 <?php endif; ?>
